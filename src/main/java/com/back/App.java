3fa2f6de23080529c6f0ef4874c456455cdae837
tobbot16 <tobbot16@naver.com>
2025-08-11 12:37:13 +0900

    AppTest t1 - red

---
+++ src/main/java/com/back/App.java
@@ -0,0 +1,7 @@
+package com.back;
+
+public class App {
+
+
+
+}
---
+++ src/test/java/com/back/AppTest.java
@@ -0,0 +1,26 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("")
+    void t1(){
+
+        Scanner sc = TestUtil.genScanner("종료");
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+        new App(sc).run();
+        String out = outputStream.toString();
+        assertThat(out).contains("=== 명언 앱 ===");
+    }
+
+
+}
