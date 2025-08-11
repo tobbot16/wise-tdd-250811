ef6c6513d8b755ec650bf1af223cfb2af06962b3
tobbot16 <tobbot16@naver.com>
2025-08-11 11:15:12 +0900

    exp1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        experiment1();
+    }
 
+
+    public static void experiment1(){
+        String input = """
+                등록
+                너 자신을 알라
+                """;
+
+        Scanner sc = new Scanner(input);
+        String cmd = sc.nextLine();
+        String saying = sc.nextLine();
+        System.out.println("입력한 명령어 : " + cmd);
+        System.out.println("입력한 명언 : " + saying);
     }
 
 }
+
+
+
