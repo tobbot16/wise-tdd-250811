608509301bee01f69966278f4ad96e75cc35b983
tobbot16 <tobbot16@naver.com>
2025-08-11 14:15:01 +0900

    step2 - green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -4,12 +4,27 @@ import java.util.Scanner;
 
 public class App {
 
-    private Scanner sc;
+    private static Scanner sc;
     public App(Scanner sc){
         this.sc = sc;
     }
     public static void run(){
         System.out.println("=== 명언 앱 ===");
+
+        while(true){
+            String cmd = sc.nextLine();
+
+            if(cmd.equals("등록")){
+                System.out.print("명령) ");
+                String saying = sc.nextLine();
+                System.out.println("명언 : ");
+                String author = sc.nextLine();
+                System.out.println("작가 : ");
+            }else if(cmd.equals("종료"))
+                break;
+        }
+
+
     }
 
 }
